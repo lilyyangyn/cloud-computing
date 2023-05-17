038290228c3e8ba6c78ffd04399f355e601dad8b
lilyyangyn <yyn605@outlook.com>
2023-05-17 17:06:51 +0200

    part4.3 & part4_4 plotting scripts and data

--- part4/plot/plot4_1.py
+++ part4/plot/plot4_1.py
@@ -2,7 +2,36 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 import numpy as np
 import argparse
-from util import *
+
+def read_file(p, filename):
+	raw_data = []
+	real_qps = []
+
+	trial_result = []
+	trial_qps = []
+	header = False
+	with open(p + "/" + filename, 'r') as f:
+		for line in f:
+			if header:
+				header = False
+				continue
+			if "End" in line:
+				raw_data.append(trial_result)
+				trial_result = []
+				real_qps.append(trial_qps)
+				trial_qps = []
+				continue
+			if "Start" in line:
+				header = True
+				continue
+			data = line.split()
+			if len(data) < 18:
+				continue
+
+			trial_result.append(float(data[12]))	# p95
+			trial_qps.append(float(data[16]))		# real qps
+
+	return raw_data, real_qps
 
 def plot(ipath, opath, save):
 	raw_data_1_1, raw_qps_1_1 = np.array(read_file(ipath, "1_1.txt")) / 1000
--- part4/plot/plot4_2.py
+++ part4/plot/plot4_2.py
@@ -2,8 +2,86 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 import numpy as np
 import argparse
-from util import *
 
+def read_file_latency(p, filename):
+	raw_data = []
+	real_qps = []
+	start_times = []
+
+	trial_result = []
+	trial_qps = []
+	start_time = []
+	header = False
+	with open(p + "/" + filename, 'r') as f:
+		for line in f:
+			if "End" in line:
+				raw_data.append(trial_result)
+				trial_result = []
+				real_qps.append(trial_qps)
+				trial_qps = []
+				start_times.append(start_time)
+				start_time = []
+				continue
+			if "Start" in line:
+				header = True
+				continue
+			data = line.split()
+			if len(data) < 18:
+				continue
+
+			if header:
+				header = False
+				continue
+
+			trial_result.append(float(data[12]))	# p95
+			trial_qps.append(float(data[16]))		# real qps
+			start_time.append(data[-2][:-3])	# start time
+
+	return raw_data, real_qps, start_times
+
+def read_file_cpu(p, filename, start_times, cores):
+	utilizations = []
+
+	tests = []
+	for i in range(cores):
+		tests.append([])
+	counter = 0
+	round_id = 0
+	total_round = len(start_times)
+	with open(p + "/" + filename, 'r') as f:
+		for line in f:
+			if "End" in line:
+				utilizations.append(tests)
+				if round_id == total_round-1:
+					return utilizations
+
+				tests = []
+				for i in range(cores):
+					tests.append([])
+				round_id += 1
+				continue
+			if "Start" in line:
+				continue
+
+			data = line.split("[")
+			if counter == 0:
+				start_time = data[0].split(".")[0]
+				
+				if start_time in start_times[round_id]:
+					counter = 5
+				else:
+					continue
+
+			values = data[-1].strip("]").split(",")
+			
+			if len(values) < cores:
+				continue
+
+			for i in range(cores):
+				tests[i].append(float(values[i]))
+			counter -= 1
+
+	return utilizations
 
 def plot(ipath, opath, save):
 	raw_data_2_1, raw_qps_2_1, start_times_2_1 = read_file_latency(ipath, "2_1_latency.txt")
@@ -14,9 +92,11 @@ def plot(ipath, opath, save):
 	x_data_2_2 = np.array(raw_qps_2_2).mean(axis=0) / 1000
 	y_data_2_2 = np.array(raw_data_2_2).mean(axis=0) / 1000
 
-	cpu_2_1_temp = np.array(read_file_cpu(ipath, "2_1_cpu.txt", start_times_2_1, 1))[0]
+	raw_cpu_2_1 = read_file_cpu(ipath, "2_1_cpu.txt", start_times_2_1, 1)
+	cpu_2_1_temp = np.array(raw_cpu_2_1)[0]
 	cpu_2_1 = np.apply_along_axis(lambda x: np.sum(x.reshape(-1, 5), axis=1)/5, 0, 
 				cpu_2_1_temp[0])
+
 	cpu_2_2_temp = np.array(read_file_cpu(ipath, "2_2_cpu.txt", start_times_2_2, 2))[0]
 	cpu_2_2 = np.apply_along_axis(lambda x: np.sum(x.reshape(-1, 5), axis=1)/5, 0, np.sum(cpu_2_2_temp, axis=0))
 	# print(cpu_2_1_temp)
@@ -32,7 +112,7 @@ def plot(ipath, opath, save):
 
 	ax1.plot(x_data_2_1, y_data_2_1, marker='o', label="95th perc. latency")
 	ax1.set_ylabel('95th Percentile Latency (ms)')
-	ax1.set_yticks(np.arange(0, 2.1, 0.2)) # 0 - 4ms
+	ax1.set_yticks(np.arange(0, 3.1, 0.2)) # 0 - 4ms
 	# ax1.yaxis.label.set_color('blue')
 	ax1.tick_params(axis='y', labelcolor='tab:blue', labelsize=12)
 
@@ -52,7 +132,7 @@ def plot(ipath, opath, save):
 
 	ax2.plot(x_data_2_2, y_data_2_2, marker='o', label="95th perc. latency")
 	ax2.set_ylabel('95th Percentile Latency (ms)')
-	ax2.set_yticks(np.arange(0, 2.1, 0.2)) # 0 - 4ms
+	ax2.set_yticks(np.arange(0, 3.1, 0.2)) # 0 - 4ms
 	ax2.tick_params(axis='y', labelcolor='tab:blue', labelsize=12)
 
 	ax2_1 = ax2.twinx()
@@ -71,14 +151,14 @@ def plot(ipath, opath, save):
 	ax1.set_xticks(np.arange(0, 125, 20)) # 0 - 125K
 	ax1.set_xlabel('Queries per second (QPS)')
 	ax1.grid(linestyle= '--')
-	ax1.legend(lines1, labels1, loc='lower right')
+	ax1.legend(lines1, labels1, loc='upper left')
 	ax1.set_title("2 threads, 1 core\n(3 repetitions per points)")
 
 	ax2.xaxis.set_major_formatter(FormatStrFormatter('%dK'))
 	ax2.set_xticks(np.arange(0, 125, 20)) # 0 - 125K
 	ax2.set_xlabel('Queries per second (QPS)')
 	ax2.grid(linestyle= '--')
-	ax2.legend(lines2, labels2, loc='lower right')
+	ax2.legend(lines2, labels2, loc='upper left')
 	ax2.set_title("2 threads, 2 cores\n(3 repetitions per points)")	
 	
 	if not save:
